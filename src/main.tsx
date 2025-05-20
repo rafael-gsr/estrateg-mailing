32c7ac09f4ab69e3d171f3f14ee524bef5976092
rafael-gsr <rafaelgomes1404@gmail.com>
2025-05-19 21:29:28 -0300

    feat(home): removing unused features and adding initial links

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { RouterProvider } from 'react-router'
+import router from './routes/index.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode>
 )
