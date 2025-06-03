795a042047cee615f301601fc1d18f271d122511
rafael-gsr <rafaelgomes1404@gmail.com>
2025-06-02 22:28:27 -0300

    feat(main): theming the main component

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router'
 import router from './routes/index.tsx'
+import './main.styles.scss'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <main className='dark-theme application-base'>
+      <RouterProvider router={router} />
+    </main>
   </StrictMode>
 )
