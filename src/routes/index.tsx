07c2f27ba239acda3373ffc51a19a1fbaae94757
rafael-gsr <rafaelgomes1404@gmail.com>
2025-06-02 22:12:42 -0300

    feat(routes): chaging the routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,17 @@
 import { createBrowserRouter } from 'react-router'
-import Clients from '../pages/Clients'
-import App from '../App'
-import Dashboards from '../pages/Dashboards'
+import Clients from '../components/Pages/Clients'
+import Dashboards from '../components/Pages/Dashboards'
+import Home from '../components/Pages/Home/Home'
 
 const router = createBrowserRouter([
-  { path: '/', Component: App, },
-  { path: 'clients', Component: Clients },
-  { path: 'dashboards', Component: Dashboards },
+  {
+    path: '/',
+    children: [
+      { index: true, Component: Home },
+      { path: 'clients', Component: Clients },
+      { path: 'dashboards', Component: Dashboards },
+    ],
+  },
 ])
 
 export default router
