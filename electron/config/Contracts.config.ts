cc46420eb57a6454d757aabdf1151c2e8996b965
rafael-gsr <rafaelgomes1404@gmail.com>
2025-08-26 23:38:32 -0300

    feat(contract-config): abstracting the contracts config

---
+++ electron/config/Contracts.config.ts
@@ -0,0 +1,45 @@
+import { IpcMainInvokeEvent } from 'electron'
+import { onHandle } from '../utils.ts'
+import { FindOptions } from 'sequelize'
+import { ContractController } from '../controllers/Contracts.controller.ts'
+import { Contract } from '../../types'
+
+export const defineContractsHandler = () => {
+  onHandle(
+    'createContract',
+    async (event: IpcMainInvokeEvent, contract: Contract) => {
+      console.log('create event: ', event)
+      const responsePromise = await ContractController.create(contract)
+      return responsePromise
+    }
+  )
+
+  onHandle(
+    'getContracts',
+    async (event: IpcMainInvokeEvent, options?: FindOptions) => {
+      console.log('get event: ', event)
+      const responsePromise = await ContractController.get(options)
+      return responsePromise
+    }
+  )
+
+  onHandle(
+    'updateContract',
+    async (event: IpcMainInvokeEvent, contract: Partial<Contract>) => {
+      console.log('update event: ', event)
+      const responsePromise = await ContractController.update(contract)
+      return responsePromise
+    }
+  )
+
+  onHandle('deleteContract', async (event: IpcMainInvokeEvent, id: string) => {
+    console.log('delete event: ', event)
+    const responsePromise = await ContractController.remove(id)
+    return responsePromise
+  })
+
+  onHandle('updateDatabase', async (event: IpcMainInvokeEvent) => {
+    console.log('update database', event)
+    return 'ok'
+  })
+}
