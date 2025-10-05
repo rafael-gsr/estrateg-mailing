ac991ff44428a42e9c2cbf5bbb509ca58e1fa7f0
rafael-gsr <rafaelgomes1404@gmail.com>
2025-10-05 13:07:47 -0300

    feat: adding the overdued contracts route and component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import { createHashRouter } from 'react-router'
 import Clients from '../components/Pages/Clients'
 import Dashboards from '../components/Pages/Dashboards'
 import Home from '../components/Pages/Home/Home'
+import OverduedContracts from '../components/Pages/OverduedContracts'
 
 const router = createHashRouter([
   {
@@ -10,6 +11,7 @@ const router = createHashRouter([
       { index: true, Component: Home },
       { path: 'clients', Component: Clients },
       { path: 'dashboards', Component: Dashboards },
+      { path: 'overdued', Component: OverduedContracts },
     ],
   },
 ])
