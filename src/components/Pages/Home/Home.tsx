6212004819cba82b76edb1fe1220b9349e563bd4
rafael-gsr <rafaelgomes1404@gmail.com>
2025-10-08 20:26:51 -0300

    fix(home): removing comments

--- src/components/Pages/Home/Home.tsx
+++ src/components/Pages/Home/Home.tsx
@@ -1,83 +1,56 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 
 import Modal from '../../Molecules/Modal'
 
-// import Button from '../../Atoms/Button'
-// import Text from '../../Atoms/Text'
-// import Title from '../../Atoms/Title'
-import BaseTemplate from '../../Templates/BaseTemplate/BaseTemplate'
-// import Input from '../../Atoms/Input/Input'
+import BaseTemplate from '../../Templates/Base/Base'
 import CreateEditContract from '../../Organisms/CreateEditContract'
 import { ContractFields } from '../../Organisms/CreateEditContract/CreateEditContract.utils'
 import { Contract } from '../../../../types'
+import Title from '../../Atoms/Title'
+import { Button } from '@mui/material'
+import { Status } from '../../../constants/Status'
 
 const Home = () => {
   const [isVisible, setIsVisible] = useState<boolean>(false)
-  // const [inputValue, setInputValue] = useState<string>()
 
   const handleOnClose = () => {
     setIsVisible(false)
   }
 
-  // const handleOpenModal = () => {
-  //   setIsVisible(true)
-  // }
-
   const onSubmit = (formValues: ContractFields) => {
-    console.log(formValues)
+    formValues.status = Status.REGULAR
 
-    window.api.createContract(formValues as Contract)
-    console.log(window.api.getContracts())
+    window.api
+      .createContract(formValues as Contract)
+      .then((response) => console.log(response))
   }
-
-  useEffect(() => {
-    console.log('execute contract')
-    // window.api.createContract({
-    //   companyName: 'meu primeiro teste',
-    //   activity: 'teste activity',
-    //   email: 'teste@email.com',
-    //   phone: '(31) 98662-8806',
-    //   dueDate: '2025-07-01T04:21:57.070Z',
-    // })
-  }, [])
 
   return (
     <BaseTemplate>
-      {/* <Title colorScheme="light" level={1}>
-        Home
+      <Title
+        level={1}
+        colorScheme='light'
+      >
+        Cadastrar novo contrato
       </Title>
 
-      <Text type="routerLink" colorScheme="light" href="clients">
-        clients
-      </Text>
-
-      <Text type="p" colorScheme="light">
-        teste
-      </Text>
-
-      <Text type="routerLink" colorScheme="dark" href="dashboards">
-        dashboards
-      </Text>
-
       <Button
-        label="dark"
-        colorScheme="dark"
-        onClick={() => {
-          handleOpenModal();
-          console.log("clicked the button");
-        }}
-      />
-      */}
+        variant='contained'
+        onClick={() => setIsVisible(true)}
+      >
+        Novo
+      </Button>
 
       <Modal
         visible={isVisible}
         onClose={handleOnClose}
-        title='hola que tal'
+        title='Novo Contrato'
       >
-        modal
+        <CreateEditContract
+          onCancel={() => setIsVisible(false)}
+          onSubmit={onSubmit}
+        />
       </Modal>
-
-      <CreateEditContract onSubmit={onSubmit} />
     </BaseTemplate>
   )
 }
