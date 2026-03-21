689eadffada81f297bcf3c23f8cbb05cd21d1c89
Rafael Gomes Souza <rafaelgomes1404@gmail.com>
2026-03-20 22:16:43 -0300

    feat(base-modal): improving the styles and applying styles to the code

--- src/components/Molecules/Modal/Modal.tsx
+++ src/components/Molecules/Modal/Modal.tsx
@@ -1,46 +1,39 @@
-import { MouseEventHandler, PropsWithChildren } from 'react'
-import './Modal.styles.scss'
-import CloseIcon from '../../Atoms/CloseIcon'
-import Title from '../../Atoms/Title'
+import { MouseEventHandler, PropsWithChildren } from "react";
+import "./Modal.styles.scss";
+import CloseIcon from "../../Atoms/CloseIcon";
+import Title from "../../Atoms/Title";
 
 type ModalProps = {
-  visible: boolean
-  onClose: () => void
-  title?: string
-} & PropsWithChildren
+  visible: boolean;
+  onClose: () => void;
+  title?: string;
+} & PropsWithChildren;
 
 const Modal = ({ children, visible, onClose, title }: ModalProps) => {
-  const handleOnClose = () => onClose()
+  const handleOnClose = () => onClose();
 
   const handleClickOutside: MouseEventHandler<HTMLDivElement> = (event) => {
     if (event.target == event.currentTarget) {
-      handleOnClose()
-      console.log('clicked outside')
+      handleOnClose();
     }
-  }
+  };
 
   if (visible)
     return (
-      <div
-        className='modal'
-        onClick={handleClickOutside}
-      >
-        <div className='modal__frame'>
+      <div className="modal" onClick={handleClickOutside}>
+        <div className="modal__frame">
           <CloseIcon onClick={handleOnClose} />
 
-          <Title
-            level={2}
-            colorScheme='light'
-          >
+          <Title level={2} colorScheme="light">
             {title}
           </Title>
 
-          {children}
+          <div>{children}</div>
         </div>
       </div>
-    )
+    );
 
-  return <></>
-}
+  return <></>;
+};
 
-export default Modal
+export default Modal;
