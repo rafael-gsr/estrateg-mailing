07f7fb71cdcb2d296674ef4e05dcb1e5a8cba99f
Rafael Gomes Souza <rafaelgomes1404@gmail.com>
2026-03-20 22:25:26 -0300

    feat(base-template): adding more providers and changing the scroll bar styles

--- src/components/Templates/Base/Base.tsx
+++ src/components/Templates/Base/Base.tsx
@@ -1,23 +1,27 @@
-import { PropsWithChildren } from 'react'
-import SideBarMenu from '../../Organisms/SideBarMenu'
+import { PropsWithChildren } from "react";
+import SideBarMenu from "../../Organisms/SideBarMenu";
 
-import './Base.styles.scss'
-import { ThemeProvider } from '@emotion/react'
-import muiTheme from '../../../styles/Theme'
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import "./Base.styles.scss";
+import { ThemeProvider } from "@emotion/react";
+import muiTheme from "../../../styles/Theme";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { SnackbarProvider } from "../../../contexts/SnackBarContext";
 
 const BaseTemplate = ({ children }: PropsWithChildren) => {
-  return (
-    <ThemeProvider theme={muiTheme}>
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <section className='base_template'>
-          <SideBarMenu />
-          <div className='base_template__content'>{children}</div>
-        </section>
-      </LocalizationProvider>
-    </ThemeProvider>
-  )
-}
+	return (
+		<ThemeProvider theme={muiTheme}>
+			<LocalizationProvider dateAdapter={AdapterDayjs}>
+				<SnackbarProvider>
+					<section className="base_template">
+						<SideBarMenu />
 
-export default BaseTemplate
+						<div className="base_template__content">{children}</div>
+					</section>
+				</SnackbarProvider>
+			</LocalizationProvider>
+		</ThemeProvider>
+	);
+};
+
+export default BaseTemplate;
