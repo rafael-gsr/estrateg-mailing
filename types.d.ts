4bf29148648dca9df8dd28ebb10565f40c2a6c3f
Rafael Gomes Souza <rafaelgomes1404@gmail.com>
2026-03-20 21:56:22 -0300

    style: applying styles

--- types.d.ts
+++ types.d.ts
@@ -1,35 +1,36 @@
-import { FindOptions } from 'sequelize'
+import { FindOptions } from "sequelize";
 
 type ElectronMethods = {
-  logData: () => void
-  logInvoke: () => Promise
+  logData: () => void;
+  logInvoke: () => Promise;
 
-  createContract: (contract: Contract) => Promise<any>
-  updateContract: (newContract: Contract) => Promise<any>
-  getContracts: (options?: FindOptions) => Promise<any>
-  deleteContract: (id: string) => Promise<any>
-  getOverduedContracts: () => Promise<Array<Contract>>
-  getOverdueThisWeek: () => Promise<Array<Contract>>
-}
+  createContract: (contract: Contract) => Promise<any>;
+  updateContract: (newContract: Contract) => Promise<any>;
+  getContracts: (options?: FindOptions) => Promise<any>;
+  deleteContract: (id: string) => Promise<any>;
+  getOverduedContracts: () => Promise<Array<Contract>>;
+  getOverdueThisWeek: () => Promise<Array<Contract>>;
+  nextState: (id: string) => Promise<Contract>;
+};
 
 type Contract = {
-  id?: string
-  companyName: string
-  activity: string
-  email: string
-  phone: string
-  dueDate: number
-  status: string
-  lastContact: string
-  observations?: string
-  createdAt?: string
-  updatedAt?: string
-}
+  id?: string;
+  companyName: string;
+  activity: string;
+  email: string;
+  phone: string;
+  dueDate: number;
+  status: string;
+  lastContact: string;
+  observations?: string;
+  createdAt?: string;
+  updatedAt?: string;
+};
 
-type ElectronMethodsKeys = keyof ElectronMethods
+type ElectronMethodsKeys = keyof ElectronMethods;
 
 declare global {
   interface Window {
-    api: ElectronMethods
+    api: ElectronMethods;
   }
 }
