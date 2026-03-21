dc49a0bde158f06f6eae29e89c5f6917bac25b31
Rafael Gomes Souza <rafaelgomes1404@gmail.com>
2026-03-20 21:59:29 -0300

    feat(status): creating the status file

---
+++ src/constants/Status.ts
@@ -0,0 +1,7 @@
+export enum Status {
+  REGULAR = "regular",
+  AROUSE_INTEREST = "arouseInterest",
+  FIRST_FOLLOW_UP = "firstFollowUp",
+  SECOND_FOLLOW_UP = "secondFollowUp",
+  BREAKUP = "breakup",
+}
