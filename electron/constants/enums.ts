f705c9a3b2b582999aaf13a9a620055a1cd0c397
Rafael Gomes Souza <rafaelgomes1404@gmail.com>
2026-03-20 21:54:59 -0300

    style: adding double quotes

--- electron/constants/enums.ts
+++ electron/constants/enums.ts
@@ -1,11 +1,12 @@
 export type AvaliableKeys =
-  | 'logData'
-  | 'logInvoke'
-  | 'new-window'
-  | 'createContract'
-  | 'updateContract'
-  | 'updateDatabase'
-  | 'deleteContract'
-  | 'getContracts'
-  | 'getOverduedContracts'
-  | 'getOverdueThisWeek'
+  | "logData"
+  | "logInvoke"
+  | "new-window"
+  | "createContract"
+  | "updateContract"
+  | "updateDatabase"
+  | "deleteContract"
+  | "getContracts"
+  | "getOverduedContracts"
+  | "getOverdueThisWeek"
+  | "nextState";
