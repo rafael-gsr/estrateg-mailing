5d79a8eeacd5fb719d377512e0bfba3f22faac8c
Rafael Gomes Souza <rafaelgomes1404@gmail.com>
2026-03-25 00:01:43 -0300

    build(vite-paths): fixing the src path bug on import

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,18 @@
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import serverConfig from "./electron/config/server.config.ts";
+import path from "node:path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
   build: {
     outDir: "dist-react",
+  },
+
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, "./src"),
+    },
   },
   server: {
     port: parseInt(serverConfig.dev.port),
